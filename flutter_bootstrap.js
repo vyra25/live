5b7d7d3bd3fa1072ce349ff483f925479c2e13a4
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2025-01-25 01:17:05 +0530

    refact: Auth UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4246112251"
+    serviceWorkerVersion: "2884250282"
   }
 });
